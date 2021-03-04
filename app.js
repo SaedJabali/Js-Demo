2ba372df1b2081aee5a2a3e69a0b552fef073851
saedjabali <saad.jabali@yahoo.com>
2021-03-04 14:05:35 +0200

    js repo

---
+++ app.js
@@ -0,0 +1,33 @@
+        console.log("Hi from ASAC");
+        document.write("Hi from ASAC");
+        console.log("Saad");
+        alert("Hi");
+       
+      var user_name = prompt("Please enter your name");
+
+        alert ("Your name is =" + user_name);
+
+        var animal = prompt("What is your fav animal");
+        confirm("Do you have another?");
+
+        if (animal == "cat")
+        {
+         document.write(animal);
+        }
+        else if(animal == "dog")
+        {
+                document.write(animal);
+        }
+      else 
+      {
+              document.write("not matched");
+      }
+
+      var userr_name = "Saad";
+      var sec_name = "Ahmad";
+
+      var Full_Name = userr_name + "_" + sec_name;
+
+      document.write(Full_Name);
+
+
